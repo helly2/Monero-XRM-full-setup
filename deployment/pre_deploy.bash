5641af0ef5411cd46e5e59cbfe7027f5d2c1ba41
helly2 <dortmundcompany@gmail.com>
2022-02-15 18:07:55 +0100

    Create pre_deploy.bash

---
+++ deployment/pre_deploy.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt update
+sudo apt-get upgrade
+sudo apt-get install -y git
+sudo apt-get install -y nano
+sudo apt-get install -y curl
+sudo apt-get install -y cmake
+sudo apt-get install -y libssl-dev
+sudo apt-get install -y pkg-config
+sudo apt-get install -y cmake libblkid-dev e2fslibs-dev libboost-all-dev libaudit-dev
+sudo apt-get install -y libzmq-dev
+sudo apt-get install -y libzmq3-dev
+sudo apt-get install -y mysql-server
+sudo apt-get install -y npm
+sudo npm install pm2 -g
